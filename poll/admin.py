267adfef136757d0d4adf0623c0a96301a8b7bcf
rohan <rohan@rohan-VPCEB46FG-W.(none)>
2014-12-19 00:41:49 +0530

    Customized change list in admin panel

--- poll/admin.py
+++ poll/admin.py
@@ -8,10 +8,13 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
 	# fields = ['pub_date','question_text']
-	fieldsets = [
-	('Question Information', {'fields':['question_text']}),
-	('Date Information',{'fields':['pub_date'],'classes':['collapse']}),
-	]
+	# fieldsets = [
+	# ('Question Information', {'fields':['question_text']}),
+	# ('Date Information',{'fields':['pub_date'],'classes':['collapse']}),
+	# ]
+	list_display = ('question_text','pub_date','was_published_recently')
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
 	inlines = [ChoiceInline]
 
 admin.site.register(Question,QuestionAdmin)
