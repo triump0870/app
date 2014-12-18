827c61b0e0c1c4d31f6b165cd8a066e7274f8544
rohan <rohan@rohan-VPCEB46FG-W.(none)>
2014-12-18 23:39:44 +0530

    customizing the admin form with tabular choice fields

--- poll/admin.py
+++ poll/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from poll.models import Question
+from poll.models import Question, Choice
 # Register your models here.
 # admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
 	# fields = ['pub_date','question_text']
 	fieldsets = [
 	('Question Information', {'fields':['question_text']}),
 	('Date Information',{'fields':['pub_date'],'classes':['collapse']}),
 	]
+	inlines = [ChoiceInline]
 
 admin.site.register(Question,QuestionAdmin)
+# admin.site.register(Choice)
