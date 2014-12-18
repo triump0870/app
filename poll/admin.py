01f69d20e2b7e94954dc07f2211c0a7e0824a71c
rohan <rohan@rohan-VPCEB46FG-W.(none)>
2014-12-18 22:05:59 +0530

    customizing the admin form with fieldsets and collapse feature

--- poll/admin.py
+++ poll/admin.py
@@ -6,7 +6,7 @@ class QuestionAdmin(admin.ModelAdmin):
 	# fields = ['pub_date','question_text']
 	fieldsets = [
 	('Question Information', {'fields':['question_text']}),
-	('Date Information',{'fields':['pub_date']}),
+	('Date Information',{'fields':['pub_date'],'classes':['collapse']}),
 	]
 
 admin.site.register(Question,QuestionAdmin)
